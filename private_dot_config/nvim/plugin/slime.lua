99738e7ac881202f15c9b94addde3e22b26364ce
Samuel Badr <samuel.badr@gmail.com>
2025-12-15 14:35:59 +0100

    Add .config/nvim/init.lua Add .config/nvim/lsp/jetls.lua Add .config/nvim/nvim-pack-lock.json Add .config/nvim/plugin/completion.lua Add .config/nvim/plugin/fff.lua Add .config/nvim/plugin/lsp.lua Add .config/nvim/plugin/options.lua Add .config/nvim/plugin/slime.lua Add .config/nvim/plugin/treesitter.lua

---
+++ private_dot_config/nvim/plugin/slime.lua
@@ -0,0 +1,8 @@
+vim.pack.add({
+    {
+        src = "https://github.com/jpalardy/vim-slime",
+    },
+})
+
+vim.g.slime_target = "tmux"
+
---
+++ private_dot_config/nvim/plugin/treesitter.lua
@@ -0,0 +1,52 @@
+vim.pack.add({
+    {
+        src = "https://github.com/nvim-treesitter/nvim-treesitter",
+        version = "main",
+    },
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+    group = vim.api.nvim_create_augroup("myconfig.treesitter", { clear = true }),
+    pattern = { "nvim-treesitter" },
+    callback = function()
+        vim.notify("Updating treesitter parsers", vim.log.levels.INFO)
+        require("nvim-treesitter").update({ summary = true }):wait(30 * 1000)
+    end,
+})
+
+require("nvim-treesitter").install({
+    "julia",
+    "zsh",
+    "lua",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = require("nvim-treesitter").get_installed(),
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
+
+local links = {
+  ["@function"]        = "Function",
+  ["@method"]          = "Function",
+  ["@keyword"]         = "Keyword",
+  ["@variable"]        = "Identifier",
+  ["@parameter"]       = "Identifier",
+  ["@field"]           = "Identifier",
+  ["@property"]        = "Identifier",
+  ["@string"]          = "String",
+  ["@string.special"]  = "Special",
+  ["@number"]          = "Number",
+  ["@boolean"]         = "Boolean",
+  ["@type"]            = "Type",
+  ["@constant"]        = "Constant",
+  ["@comment"]         = "Comment",
+  ["@operator"]        = "Operator",
+  ["@punctuation"]     = "Delimiter",
+}
+
+for capture, group in pairs(links) do
+  vim.api.nvim_set_hl(0, capture, { link = group })
+end
+
