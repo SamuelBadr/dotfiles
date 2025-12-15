4764b603b5a752ce4f0aa8ae77b2587c7268f129
Samuel Badr <samuel.badr@gmail.com>
2025-12-15 23:20:07 +0100

    Update .zshrc Update .config/nvim/nvim-pack-lock.json Update .config/nvim/plugin/fff.lua Update .config/nvim/plugin/treesitter.lua

--- private_dot_config/nvim/plugin/fff.lua
+++ private_dot_config/nvim/plugin/fff.lua
@@ -1,46 +1,46 @@
-vim.pack.add({ 'https://github.com/dmtrKovalenko/fff.nvim' })
-
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(event)
-    if event.data.updated then
-      require('fff.download').download_or_build_binary()
-    end
-  end,
-})
-
--- the plugin will automatically lazy load
-vim.g.fff = {
-  lazy_sync = true, -- start syncing only when the picker is open
-  debug = {
-    enabled = true,
-    show_scores = true,
-  },
-}
-
-vim.keymap.set(
-  'n',
-  'ff',
-  function() require('fff').find_files() end,
-  { desc = 'FFFind files' }
-)
-
--- https://github.com/dmtrKovalenko/fff.nvim/issues/64#issuecomment-3229729261
-vim.api.nvim_create_autocmd("PackChanged", {
-	callback = function(ev)
-		local spec = ev.data.spec
-		if spec and spec.name == "fff.nvim" and ev.data.kind == "install" or ev.data.kind == "update" then
-			local fff_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/fff.nvim"
-			vim.fn.jobstart({ "cargo", "build", "--release" }, {
-				cwd = fff_path,
-				on_exit = function(_, code)
-					if code == 0 then
-						vim.notify("Cargo build finished successfully in " .. fff_path, vim.log.levels.INFO)
-					else
-						vim.notify("Cargo build failed with exit code " .. code, vim.log.levels.ERROR)
-					end
-				end,
-			})
-		end
-	end,
-})
-
+-- vim.pack.add({ 'https://github.com/dmtrKovalenko/fff.nvim' })
+--
+-- -- https://github.com/dmtrKovalenko/fff.nvim/issues/64#issuecomment-3229729261
+-- vim.api.nvim_create_autocmd("PackChanged", {
+-- 	callback = function(ev)
+-- 		local spec = ev.data.spec
+-- 		if spec and spec.name == "fff.nvim" and ev.data.kind == "install" or ev.data.kind == "update" then
+-- 			local fff_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/fff.nvim"
+-- 			vim.fn.jobstart({ "cargo", "build", "--release" }, {
+-- 				cwd = fff_path,
+-- 				on_exit = function(_, code)
+-- 					if code == 0 then
+-- 						vim.notify("Cargo build finished successfully in " .. fff_path, vim.log.levels.INFO)
+-- 					else
+-- 						vim.notify("Cargo build failed with exit code " .. code, vim.log.levels.ERROR)
+-- 					end
+-- 				end,
+-- 			})
+-- 		end
+-- 	end,
+-- })
+--
+-- vim.api.nvim_create_autocmd('PackChanged', {
+--   callback = function(event)
+--     if event.data.updated then
+--       require('fff.download').download_or_build_binary()
+--     end
+--   end,
+-- })
+--
+-- -- the plugin will automatically lazy load
+-- vim.g.fff = {
+--   lazy_sync = true, -- start syncing only when the picker is open
+--   debug = {
+--     enabled = true,
+--     show_scores = true,
+--   },
+-- }
+--
+-- vim.keymap.set(
+--   'n',
+--   'ff',
+--   function() require('fff').find_files() end,
+--   { desc = 'FFFind files' }
+-- )
+--
