c97dd01703ab0691bb392e92ee00bbd1fb96dc5f
Samuel Badr <samuel.badr@gmail.com>
2026-09-08 11:30:12 +0200

    Flatten starship + macos-keyboard gates into .chezmoiignore

--- .chezmoiscripts/run_once_after_macos-keyboard.sh
+++ .chezmoiscripts/run_once_after_macos-keyboard.sh
@@ -1,8 +1,6 @@
-{{- if and (eq .chezmoi.os "darwin") .modules.workstation -}}
 #!/bin/sh
 set -eu
 # Apply once per machine, never during shell startup.
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-{{- end }}
